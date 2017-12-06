cdf3f96193fe1a77d5fc441c00547cb453e1f258
Celestial Paler <31444144+CelestialTS@users.noreply.github.com>
2017-12-06 23:34:00 +0800

    Almost working right now! XD

--- toolfuncs.py
+++ toolfuncs.py
@@ -13,25 +13,66 @@ import os
 
 
 # Check if it is a valid path
-# Return : Boolean
-def __ispath_valid(path):
+# Return : True or Error
+def is_path_valid(path):
     if os.path.exists(path):
         return True
     else:
-        raise Exception('ERROR : Invalid Image Diration!')
+        raise Exception('ERROR : Invalid Directory!')
 
 
-# Get all the folder in the root folder
-# Args : None
-# Return : None
-def __get_all_folder(self):
-    root = self.__image_dir_root
-    dir_list = [item for item in os.listdir(root) if os.path.isdir(os.path.join(root, item))]
-    self.__classes_names = dir_list
+# Get all the sub folder`s name in a root folder
+# Args : folder_dir = a abspath to the target folder
+# Return : list()
+def get_all_folder(folder_dir):
+    temp_list = [item for item in os.listdir(folder_dir)
+                 if os.path.isdir(os.path.join(folder_dir, item))]
+    return temp_list
 
 
-# Get all image files in the folder
-# Args : None
-# Return : None
-def __get_all_image(self):
-    print(self.__classes_names.__sizeof__())
+# Get all image files in the folder1
+# Args : folder_dir = a abspath to the target folder
+# Return : list()
+def get_all_image(folder_dir):
+    temp_list = list()
+    for file in os.listdir(folder_dir):
+        if file.endswith(".jpg"):
+            temp_list.append(file)
+    return temp_list
+
+
+# Get all the image files in the folder
+# Args : folder_dir = a abspath to the target folder
+# Return : dict()
+def get_all_image_dicts(folder_dir):
+    temp_dict = dict()
+    names = get_all_folder(folder_dir)
+    for name in names:
+        href_dir = '/' + str(name)
+        templist = sorted(get_all_image(root_dir + href_dir))
+        temp_dict.update({name: templist})
+    return temp_dict
+
+
+# Create target folders to contain the csv files of each classes
+def create_target_folders(folder_dir):
+    if os.path.exists(folder_dir):
+        names = get_all_folder(root_dir)
+        for name in names:
+            href_dir = '/' + str(name)
+            try:
+                os.makedirs(folder_dir + href_dir)
+            except OSError:
+                pass
+                # raise Exception('ERROR : Target Directory Already Exits!')
+    else:
+        raise Exception('ERROR : Invalid Target Directory!')
+
+
+# Debug Script
+if __name__ == '__main__':
+    root_dir = '/home/celestial/Documents/PythonProject/ImagesetGenerator/TestImages'
+    target_dir = '/home/celestial/Documents/PythonProject/ImagesetGenerator/TestCSV'
+    images = get_all_image_dicts(root_dir)
+    create_target_folders(target_dir)
+    print(images)
